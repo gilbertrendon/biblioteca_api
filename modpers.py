10f1daa3c898f30cb4b91715e94e5cf03f208ee4
gilbertrendon <jilberlv@hotmail.com>
2025-03-06 13:47:46 -0500

    ya tengo el crud completo en el repositorio

--- modpers.py
+++ modpers.py
@@ -10,7 +10,8 @@ engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 # Define la base para modelos declarativos
 Base = declarative_base()
-
+SessionLocal = sessionmaker(bind=engine)
+session = SessionLocal()
 # Define tu modelo de datos
 class Book(Base):
     __tablename__ = "books"
@@ -24,21 +25,70 @@ class Book(Base):
     def __str__(self):
         return self.title
     
+        
     @staticmethod
-    def createTable(asdf):
-        SessionLocal = sessionmaker(bind=asdf)
-        session = SessionLocal()
+    def createTable(testengine):
         # se borra la tabla por si existe 
-        Base.metadata.drop_all(engine)
+        Base.metadata.drop_all(testengine)
         # Crea las tablas
-        Base.metadata.create_all(engine)
+        Base.metadata.create_all(testengine)
+        # session.commit()
+        # session.close()
+        
+    @staticmethod
+    def create_book(lb):
+        session.add(lb)
+    
+    @staticmethod
+    def delete_book(idb):
+        book_to_delete = session.query(Book).filter_by(id=idb).first()
+        session.delete(book_to_delete)
+
+    @staticmethod
+    def get_book(idb):
+        book= session.query(Book).filter_by(id=idb).first()
+        print(book)
+        return book
+        
+    @staticmethod
+    def update_book(book):
+        book_to_update = session.query(Book).filter_by(id=book.id).first()
+        book_to_update.title = book.title
+        book_to_update.author = book.autor
+        book_to_update.year = book.year
+        book_to_update.isbn = book.isbn
+        session.add(book_to_update)
+        session.commit()
+        
+if __name__ == '__main__':
+        lib = Book(
+        title="Harry potter y el caliz de fuego",
+        autor="Jk rowling",
+        year="2019-12-04",
+        isbn="e5d4"
+        )
+    
+        # SessionLocal = sessionmaker(bind=engine)
+        # session = SessionLocal()
+        # CREAR TABLA
+        # lib.createTable(engine)
+        # INSERTAR UN LIBRO
+        # lib.create_book(lib)
+        # BORRAR UN LIBRO
+        # lib.delete_book(1)
+        # CONSULTAR UN LIBRO POR ID
+        # lib.get_book(2)
+        # ACTUALIZAR UN LIBRO
+        libro = lib.get_book(2)
+
+    # MODIFICAR LOS ATRIBUTOS
+        libro.title = 'Harry Potter y el cáliz de fuego'
+        libro.autor = 'Jk Rowling2'
+        libro.year = '2023-11-04'
+        libro.isbn = 'isbn2'
+        lib.update_book(libro)
+        
         session.commit()
         session.close()  
-
-
-if __name__ == '__main__':
-        lib = Book()
-        lib.createTable(engine)
-       
   
     
